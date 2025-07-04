0b267bf16d0f9e284056a1998b68cbfa03c4db36
faiqalharits <alharistfaiq10@gmail.com>
2025-07-04 23:56:06 +0700

    done

---
+++ Js/data.js
@@ -0,0 +1,68 @@
+const armada = [
+  {
+    name : "Avanza",
+    image : "/assets/images/car/Toyota-Avanza.png",
+    harga: "500.000",
+    seat : 6
+  },
+  {
+    name : "Expander",
+    image : "/assets/images/car/Mitsubishi-Xpander.png",
+    harga: "650.000",
+    seat : 6
+  },
+  {
+    name : "Innova Reborn",
+    image : "/assets/images/car/innova_reborn.png",
+    harga: "750.000",
+    seat : 6
+  },
+  {
+    name : "Innova Zenix",
+    image : "/assets/images/car/Innova_Zenix.png",    
+    harga: "950.000",
+    seat : 6
+  },
+  {
+    name : "Fortuner",
+    image : "/assets/images/car/Toyota-Fortuner.png",
+    harga : "1.500.000",
+    seat : 6
+  },
+  {
+    name : "Pajero",
+    image : "/assets/images/car/Pajero-Sport.png",
+    harga: "1.700.000",
+    seat : 6
+  },
+  {
+    name : "Alphard Euro 4",
+    image : "/assets/images/car/Alphard_Euro4.png",
+    harga: "3.600.000",
+    seat : 6
+  },
+  {
+    name : "Hiace Commuter",
+    image : "/assets/images/car/Hiace_Commuter.png",
+    harga: "1.550.000",
+    seat : 14
+  },
+  {
+    name : "Hiace Premio",
+    image : "/assets/images/car/Hiace_Premio.png",
+    harga: "1.700.000",
+    seat : 12
+  },
+  {
+    name : "Toyota Alphard",
+    image : "/assets/images/car/Toyota_Alphard.png",
+    harga: "2.600.000",
+    seat : 6
+  },
+  {
+    name : "New Avanza Veloz",
+    image : "/assets/images/car/Avanza_Veloz.png",
+    harga: "650.000",
+    seat : 6
+  }
+];
